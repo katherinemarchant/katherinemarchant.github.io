cf06b7feea83281f9f64db4549b0271b5c4b638a
Matthew Hinz <43189125+mhinzz@users.noreply.github.com>
2026-05-15 02:47:15 -0400

    colour default switch

--- script.js
+++ script.js
@@ -24,21 +24,21 @@ function initialiseThemeToggle() {
   
 	const savedTheme = localStorage.getItem("theme");
   
-	if (savedTheme === "light") {
-	  document.body.classList.add("light-mode");
+	if (savedTheme === "dark") {
+	  document.body.classList.add("dark-mode");
 	  toggle.checked = true;
 	} else {
-	  document.body.classList.remove("light-mode");
+	  document.body.classList.remove("dark-mode");
 	  toggle.checked = false;
 	}
   
 	toggle.addEventListener("change", () => {
 	  if (toggle.checked) {
-		document.body.classList.add("light-mode");
-		localStorage.setItem("theme", "light");
-	  } else {
-		document.body.classList.remove("light-mode");
+		document.body.classList.add("dark-mode");
 		localStorage.setItem("theme", "dark");
+	  } else {
+		document.body.classList.remove("dark-mode");
+		localStorage.setItem("theme", "light");
 	  }
 	});
   }
